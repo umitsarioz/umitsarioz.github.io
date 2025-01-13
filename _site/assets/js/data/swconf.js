be8072e8979e4f8dddba1fd433f145d5c08298c8
Ümit Sarıöz <umitsariozz@gmail.com>
2025-01-13 23:52:29 +0300

    site installed

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1736801534',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/blog-timeline/',
+      
+        '/projects/',
+      
+        '/tags/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+          'https://umitsarioz.goatcounter.com/counter/'
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
